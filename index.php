2b463f8ca1715c6486fb3829d119600b6c3acc93
elikimchi <eli.kimchi@pscs.org>
2018-01-23 08:56:23 -0800

    Added favicon, changed index.html back to php
    Favicon will not show in mamp, however if you go to mamp’s local files
    -> bin and replace the mamp .ico file with your own it will change.

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PSCS Attendance 3.0</title>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+
+  </head>
+  <body>
+  </body>
+</html>
