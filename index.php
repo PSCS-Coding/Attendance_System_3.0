263f1795cfc196e65bd0dad06590ad1880f35a3d
eta57 <theeta57@gmail.com>
2018-01-29 11:51:50 -0800

    fixing all the problems
    it is perfect

--- index.php
+++ index.php
@@ -1,10 +1,45 @@
+<?php
+session_start();
+?>
+<?php
+function status_update($student, $status, $old_status)
+{
+	$query = 'UPDATE current_stati SET status_id = '.enqute($status).' WHERE student_id = '.enquote($student);
+	$stati = $db->query($query);
+
+
+    return 0;
+}
+function enquote($text){
+	return '"'.$text.'"';
+}
+?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>PSCS Attendance 3.0</title>
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-
-  </head>
-  <body>
-  </body>
+  	<head>
+    	<title>
+			User Panel: Free Commerce For Sale
+    	</title>
+	    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    	<link rel="stylesheet" type="text/css" href="style.css">
+  	</head>
+	<body>
+    	<?php
+		$db = new mysqli('localhost:8889', 'root', 'root', 'attendance_new');
+		if($db->connect_errno > 0){
+			echo 'fail';
+		    die('Unable to connect to database [' . $db->connect_error . ']');
+		}
+		$query = 'SELECT * FROM current_stati INNER JOIN students ON current_stati.student_id = students.student_id INNER JOIN status ON current_stati.status_id = status.status_id';
+		$stati = $db->query($query)->fetch_all($resulttype = MYSQLI_ASSOC);
+		if($_GET['button'] = 'yes'){
+			echo 'button';
+		}
+		echo '<table><tr><th>Student</th><th>Status</th></tr>';
+		foreach($stati as &$row){
+			echo '<tr><td>'.$row["first_name"].' '.$row["last_name"][0].'.</td><td><a href="/index.php?button=yes&student='.$row["student_id"].'">'.$row["status_name"].'</a></td></tr>';
+		}
+		echo '</table>';
+		?>
+	</body>
 </html>
--- readme.md
+++
@@ -1,7 +0,0 @@
-# PSCS Attendance system
-
-A complete rewrite of the [school's](http://pscs.org) system for tracking student attendance.
-
-## Team
-
-Angus, Anthony, Caden, Eli, Henry, Jack, Sam, Simon, and Nic
