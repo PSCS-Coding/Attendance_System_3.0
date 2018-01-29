e394722e4eae13140a82cb45955367cc2855b2ad
jacknote <jack.notarangelo@pscs.org>
2018-01-29 14:21:03 -0800

    made it better

--- jacks_front_page.php
+++ jacks_front_page.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="style.css"/>
     <title>First Page</title>
   </head>
   <body>
-    <?php $db = new mysqli('localhost', 'root', '', 'Name');
+    <?php $db = new mysqli('localhost', 'root', '', 'attendance_new');
   if($db->connect_errno > 0){
     die("<div class='center'><h1>Sorry, but we can't seem to connect to our database right now. Try again later.</h1></div>");
   }
@@ -13,21 +14,33 @@
 
   $fnames = [];
   $lnames = [];
+  $id = [];
 
 
   foreach ($stmt->get_result() as $row){
     $fnames[] = $row["first_name"];
-    $lnames[] = $row["last_name"]
+    $lnames[] = $row["last_name"];
+    $id[] = $row["student_id"];
   }?>
       <table>
         <tr>
           <th>First Name</th>
           <th>Last Name</th>
+          <th></th>
         </tr>
         <?php
-          for ($i = 0; $i < count($names); $i++) {
-            echo "<tr><td>" . $lnames[$i] . "</td>";
-            echo "<td>" . $fnames[$i] . "</td><tr>";
+          for ($i = 0; $i < count($fnames); $i++) {
+            echo "<tr><td>" . $fnames[$i] . "</td>";
+            echo "<td>" . $lnames[$i] . "</td>";
+            ?>
+            <td>
+            <form action="first_page.php" method="post">
+              <input type="submit" name="present_button" value="P">
+            </form></td></tr> <?php
+            if(isset($_POST["present_button"])) {
+              $stmt = $db->query("UPDATE current_stati SET status_id = 2 WHERE student_id = ". $id[$i] ."");
+            }
+            unset($_POST["present_button"]);
         }
         ?>
       </table>
