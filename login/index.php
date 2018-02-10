305374fe8f7b0fb74b14dbe3c0f90ce7b7b6dc95
Milo Rupp <badtzmaru139@gmail.com>
2018-02-10 15:26:34 -0800

    fixed some tabs

--- login/index.php
+++ login/index.php
@@ -13,14 +13,13 @@
 			}
 		?>
 		<div class='section'>
-
 			<p>Students or staff:</p>
 			<div class="g-signin2" data-onsuccess="onSignIn"></div>
 		</div>
 		<div class='section'>
 			<p>Password sign-in:</p>
 			<form name="login" method="post" action="#">
-    	<input name="pass" type="password" placeholder="Password" required>
+    			<input name="pass" type="password" placeholder="Password" required>
 			<input type="submit" name="submit" value="Sign In">
 		</form>
 		</div>
@@ -32,28 +31,26 @@
 			    return (xmlHttp.responseText);
 			}
 			function signOut() {
-				var auth2 = gapi.auth2.getAuthInstance();
-				auth2.signOut().then(function () {
-					console.log('User signed out.');
-				});
+				gapi.auth2.getAuthInstance().disconnect();
+				window.location.href = window.location.href;
 			}
 			function onSignIn(googleUser) {
 		  		var profile = googleUser.getBasicProfile();
 		  		var authresult = sendUserData(profile.getName(), profile.getImageUrl());
 		  		if(authresult >= 1) {
-						console.log(authresult);
-						if(window.location.href.includes("?to=")){
+					console.log(authresult);
+					if(window.location.href.includes("?to=")){
 		  				window.location.href = window.location.href.split("?to=")[1];
-							console.log(window.location.href.split("?to=")[1]);
-						} else {
-							window.location.href = "/test.php"; //change to be index
-						}
-		  		} else {
-							signOut();
-							alert("It looks like you're not a user in our database. Please try a different Google account or use password login.");
-							console.log(0);
+						console.log(window.location.href.split("?to=")[1]);
+					} else {
+						window.location.href = "/test.php"; //change to be index
 					}
+		  		} else {
+					signOut();
+					alert("It looks like you're not a user in our database. Please try a different Google account or use password login.");
+					console.log(0);
 				}
+			}
     </script>
 </head>
 </html>
