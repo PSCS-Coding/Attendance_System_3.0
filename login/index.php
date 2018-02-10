818666ea5118aa083e8b1c3a8362ffeaed13a7c8
elikimchi <eli.kimchi@pscs.org>
2018-02-09 19:23:02 -0800

    fixed stupider error

--- login/index.php
+++ login/index.php
@@ -38,25 +38,6 @@
 		  			console.log("failure");
 		  		}
 			}
-
-		
-
-        function onSignIn(googleUser) {
-            var profile = googleUser.getBasicProfile();
-            var authresult = sendUserData(profile.getName(), profile.getImageUrl());
-            if (authresult == "true") {
-                console.log("success");
-            } else {
-                console.log("failure");
-            }
-        }
-
-        function signOut() {
-            var auth2 = gapi.auth2.getAuthInstance();
-            auth2.signOut().then(function() {
-                console.log('User signed out.');
-            });
-        }
     </script>
 </head>
 
