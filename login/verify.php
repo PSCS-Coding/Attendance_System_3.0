8c2ef7b4eb9d573c3c44dce18e510f61f3d45f69
Milo Rupp <badtzmaru139@gmail.com>
2018-02-10 13:08:10 -0800

    changed row count function

--- login/verify.php
+++ login/verify.php
@@ -1,12 +1,12 @@
 <?php
-  require_once('connection.php');
+  require_once("/login/connection.php");
   if(!empty($_COOKIE['user'])) {
     $user = str_replace("+", " ", $_COOKIE['user']);
-    $studentQuery = $db->query('SELECT full_name FROM student_data WHERE full_name = "$user"');
-    $row_cnt = $studentQuery->num_rows();
+    $studentQuery = $db->query("SELECT full_name FROM student_data WHERE full_name = '$user'");
+    $row_cnt = mysqli_num_rows($studentQuery);
     if($row_cnt == 0) {
-      $adminQuery = $db->query('SELECT full_name FROM admins WHERE full_name = "$user"');
-      $row_cnt = $adminQuery->num_rows();
+      $adminQuery = $db->query("SELECT full_name FROM admins WHERE full_name = '$user'");
+      $row_cnt = mysqli_num_rows($adminQuery);
       if($row_cnt == 0) {
         header('Location: login/index.php');
       }
