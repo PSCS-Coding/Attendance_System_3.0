b01f7940b685a57aee892731b414dad4dbd838c7
elikimchi <eli.kimchi@pscs.org>
2018-02-10 22:09:56 -0800

    Added password verification, but it doesn't set the cookie in index.
    I’ll work on it tomorrow.

--- login/verify.php
+++ login/verify.php
@@ -1,17 +1,28 @@
 <?php
-  require_once('login/connection.php');
-  if(!empty($_COOKIE['user'])) {
+  $db = mysqli_connect("localhost", "root", "root", "pscsorg_3.0");
+  if(!empty($_COOKIE['user']) || $_COOKIE['login']) {
     $user = $_COOKIE['user'];
+    $year = date(Y);
+    $loginQuery = $db->prepare("SELECT login_password FROM login WHERE login_year = '$year'");
+    $loginQuery->execute();
+    $loginResult = Array();
+    foreach ($loginQuery->get_result() as $row)
+    {
+      array_push($loginResult, $row['login_password']);
+    }
+    if($_COOKIE['login'] != $loginResult[0]) {
+      header('Location: login/index.php?to=' . $_SERVER['REQUEST_URI']);
+    }
     $studentQuery = $db->query("SELECT imgurl FROM student_data WHERE imgurl = '$user'");
     $row_cnt = mysqli_num_rows($studentQuery);
     if($row_cnt == 0) {
       $adminQuery = $db->query("SELECT imgurl FROM admins WHERE imgurl = '$user'");
       $row_cnt = mysqli_num_rows($adminQuery);
       if($row_cnt == 0) {
-        header('Location: login/index.php?to=' . $_SERVER['REQUEST_URI']);
+        header('Location: /login/index.php?to=' . $_SERVER['REQUEST_URI']);
       }
     }
   } else {
-    header('Location: login/index.php?to=' . $_SERVER['REQUEST_URI']);
+    header('Location: /login/index.php?to=' . $_SERVER['REQUEST_URI']);
   }
 ?>
