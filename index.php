a07ffaf002d428683f56ff1f4c857df24defc784
eta57 <theeta57@gmail.com>
2018-02-12 11:10:06 -0800

    fixed error on line 31
    fixed database

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once("connection.php");
 function status_update($student, $status, $old_status)
 {
 	global $db;
-	$query = 'UPDATE current_stati SET status_id = '.$status.' WHERE student_id = '.$student;
+	$query = 'UPDATE current SET status_id = '.$status.' WHERE student_id = '.$student;
 	$db->query($query);
     return 0;
 }
@@ -27,11 +27,11 @@ function enquote($text){
   	</head>
 	<body>
     	<?php
-		$query = 'SELECT * FROM current_stati INNER JOIN students ON current_stati.student_id = students.student_id INNER JOIN status ON current_stati.status_id = status.status_id ORDER BY first_name DESC';
+		$query = 'SELECT * FROM current INNER JOIN student_data ON current.student_id = student_data.student_id INNER JOIN status_data ON current.status_id = status_data.status_id ORDER BY first_name DESC';
 		$stati = $db->query($query)->fetch_all($resulttype = MYSQLI_ASSOC);
 		if (!empty($_POST['change'])) {
 			status_update($_POST['student'],$_POST['new'] , $_POST['current']);
-			$query = 'SELECT * FROM current_stati INNER JOIN students ON current_stati.student_id = students.student_id INNER JOIN status ON current_stati.status_id = status.status_id ORDER BY first_name DESC';
+			$query = 'SELECT * FROM current INNER JOIN student_data ON current.student_id = student_data.student_id INNER JOIN status_data ON current.status_id = status_data.status_id ORDER BY first_name DESC';
 			$stati = $db->query($query)->fetch_all($resulttype = MYSQLI_ASSOC);
 
 		}
@@ -39,14 +39,14 @@ function enquote($text){
 		foreach($stati as &$row){
 			echo '<tr><td>'.$row["first_name"].' '.$row["last_name"][0].'.</td><td><p>'.$row["status_name"].' </p> <form action="/index.php" method="POST">';
 			echo '<input type="hidden" name="student" value="'.$row["student_id"].'"> <input type=hidden name=current value="'.$row["status_id"].'">';
-			if($row['status_id'] != 2 ){
-				echo '<input type="hidden" name="new" value=2> <input type="submit" name="change" value="P">';
-				if($row['status_id'] != 7 && $row['status_id'] != 5)
+			if($row['status_id'] != 1 ){
+				echo '<input type="hidden" name="new" value=1> <input type="submit" name="change" value="P">';
+				if($row['status_id'] != 7  && $row['status_id'] != 4)
 					echo '<input type="hidden" name="new" value=7> <input type="submit" name="change" value="A">';
 			}
 			else{
-				if($row['status_id'] != 5)
-					echo '<input type="hidden" name="new" value=5> <input type="submit" name="change" value="SO">';
+				if($row['status_id'] != 4 )
+					echo '<input type="hidden" name="new" value=4> <input type="submit" name="change" value="CO">';
 			}
 			echo '</form></td></tr>';
 		}
