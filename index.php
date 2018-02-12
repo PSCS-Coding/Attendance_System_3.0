afbd9e2aed53ce9b27ecd6fd757f742b00b3a049
eta57 <theeta57@gmail.com>
2018-02-12 11:48:32 -0800

    cool
    slightly different

--- index.php
+++ index.php
@@ -8,8 +8,13 @@ require_once("connection.php");
 function status_update($student, $status, $old_status)
 {
 	global $db;
-	$query = 'UPDATE current SET status_id = '.$status.' WHERE student_id = '.$student;
-	$db->query($query);
+	if($student == 'DAILY_RESET'){
+		$query = 'UPDATE current SET status_id = '.$status;
+		$db->query($query);
+	}else{
+		$query = 'UPDATE current SET status_id = '.$status.' WHERE student_id = '.$student;
+		$db->query($query);
+	}
     return 0;
 }
 function enquote($text){
@@ -29,7 +34,9 @@ function enquote($text){
     	<?php
 		if (!empty($_POST['change']))
 			status_update($_POST['student'],$_POST['new'] , $_POST['current']);
-		echo '<table><tr><th>Student</th><th>Status</th></tr>';
+		echo '<form action="/index.php" method="POST"><input type="hidden" name="student" value="DAILY_RESET"> <input type=hidden name=current value="0"><input type="hidden" name="new" value=0> <input type="submit" name="change" value="Set all to \'Not checked in\'"></form>
+
+		<table><tr><th>Student</th><th>Status</th></tr>';
 		$query = 'SELECT * FROM current INNER JOIN student_data ON current.student_id = student_data.student_id INNER JOIN status_data ON current.status_id = status_data.status_id ORDER BY first_name DESC';
 		$stati = $db->query($query)->fetch_all($resulttype = MYSQLI_ASSOC);
 		foreach($stati as &$row){
