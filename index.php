ea31aef24c985445925a3a7ac5c9b0bb45d9e5da
eta57 <theeta57@gmail.com>
2018-02-12 13:01:08 -0800

    fixed forms
    working buttons

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ function enquote($text){
 <html>
   	<head>
     	<title>
-			Attendance System 100% Complete Perfect No Virus Download Free Effective and Efficient
+			Atendance Sistim 100% Compleet Perfict No Virus Downlode Free Affective end Afficient Profetional Git it Now Easy Set Up Aply Today Has Enyone Really Been Far Even as Descided to Use Evin Go Wunt to do Look Mor Like Go Further You Can Realy be Far It's Just Commin Sense
     	</title>
 	    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
     	<link rel="stylesheet" type="text/css" href="style.css">
@@ -40,18 +40,17 @@ function enquote($text){
 		$query = 'SELECT * FROM current INNER JOIN student_data ON current.student_id = student_data.student_id INNER JOIN status_data ON current.status_id = status_data.status_id ORDER BY first_name DESC';
 		$stati = $db->query($query)->fetch_all($resulttype = MYSQLI_ASSOC);
 		foreach($stati as &$row){
-			echo '<tr><td>'.$row["first_name"].' '.$row["last_name"][0].'.</td><td><p>'.$row["status_name"].' </p> <form action="/index.php" method="POST">';
-			echo '<input type="hidden" name="student" value="'.$row["student_id"].'"> <input type=hidden name=current value="'.$row["status_id"].'">';
+			echo '<tr><td>'.$row["first_name"].' '.$row["last_name"][0].'.</td><td><p>'.$row["status_name"].' </p>';
 			if($row['status_id'] != 1 ){
-				echo '<input type="hidden" name="new" value=1> <input type="submit" name="change" value="P">';
+				echo '<form action="/index.php" method="POST"> <input type="hidden" name="student" value="'.$row["student_id"].'"> <input type=hidden name=current value="'.$row["status_id"].'"><input type="hidden" name="new" value=1> <input type="submit" name="change" value="P"></form>';
 				if($row['status_id'] != 7  && $row['status_id'] != 4)
-					echo '<input type="hidden" name="new" value=7> <input type="submit" name="change" value="A">';
+					echo '<form action="/index.php" method="POST"> <input type="hidden" name="student" value="'.$row["student_id"].'"> <input type=hidden name=current value="'.$row["status_id"].'"><input type="hidden" name="new" value=7> <input type="submit" name="change" value="A"></form>';
 			}
 			else{
 				if($row['status_id'] != 4 )
-					echo '<input type="hidden" name="new" value=4> <input type="submit" name="change" value="CO">';
+					echo '<form action="/index.php" method="POST"> <input type="hidden" name="student" value="'.$row["student_id"].'"> <input type=hidden name=current value="'.$row["status_id"].'"><input type="hidden" name="new" value=4> <input type="submit" name="change" value="CO"></form>';
 			}
-			echo '</form></td></tr>';
+			echo '</td></tr>';
 		}
 		echo '</table>';
 		?>
