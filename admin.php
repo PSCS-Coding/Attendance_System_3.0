a671751ad7e8509c9f0697ff47aeb5ea75f42076
eta57 <theeta57@gmail.com>
2018-03-12 14:31:45 -0700

    update

--- admin.php
+++ admin.php
@@ -1,3 +1,6 @@
+<?php
+require_once("connection.php");
+?>
 <!DOCTYPE html>
 
 <html>
@@ -12,48 +15,67 @@
 <body>
 	<div>
 		<?php
+		$goodpage = false;
 		//Allotted Hours
 		if((string)$_GET['page'] == "0"){
-			
+			$goodpage = True;
+			$index = array('veteran_year','default_offsite','default_is');
+			$query = 'SELECT * FROM allotted_hours;';
 		}
 		//Current Events
 		elseif((string)$_GET['page'] == "1"){
-
+			$goodpage = True;
 		}
 		//Facilitator Edit View
 		elseif((string)$_GET['page'] == "2"){
-
+			$goodpage = True;
 		}
 		//Group Edit View
 		elseif((string)$_GET['page'] == "3"){
-
+			$goodpage = True;
 		}
 		//History
 		elseif((string)$_GET['page'] == "4"){
-
+			$goodpage = True;
 		}
 		//Holidays
 		elseif((string)$_GET['page'] == "5"){
-
+			$goodpage = True;
 		}
 		//Offsite Locations
 		elseif((string)$_GET['page'] == "6"){
-
+			$goodpage = True;
 		}
 		//Passwords
 		elseif((string)$_GET['page'] == "7"){
-
+			$goodpage = True;
 		}
 		//School Hours
 		elseif((string)$_GET['page'] == "8"){
-
+			$goodpage = True;
 		}
 		//Student Edit View
 		elseif((string)$_GET['page'] == "9"){
-
+			$goodpage = True;
 		}
 		else{
 			echo "<h1>Bad URL!</h1>";
+		}if($goodpage){
+			echo '<table><tr>';
+			foreach($index as &$header){
+				echo '<th>'.str_replace('_', ' ',$header).'</th>';
+			}
+			$values = $db->query($query)->fetch_all($resulttype = MYSQLI_ASSOC);
+			echo '</tr>';
+			print_r($values);
+			foreach($values as &$value){
+				echo '<tr>';
+				foreach($index as &$oi){
+					echo '<td>'.$value[$oi].'<td>';
+				}
+				echo '</tr>';
+			}
+			echo '</table>';
 		}
 		 ?>
 	</div>
