d31dcae8584516ed1affc0f9c4467a8eb788cc95
Anthony <anthony.reyes@pscs.org>
2018-03-12 09:33:33 -0700

    Add Header File & Testing for Index CSS

--- index.php
+++ index.php
@@ -1,46 +1,72 @@
 <?php
 session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require_once("connection.php");
-require_once("functions.php");
+//incomplete status update function
+function status_update($student, $status, $old_status, $return_time)
+{
+	global $db;
+	if($student == 'DAILY_RESET'){
+		$query = 'UPDATE current SET status_id = '.$status.', return_time = '.$return_time;
+		$db->query($query);
+	}else{
+		if($return_time!= Null){
+			$query = 'UPDATE current SET status_id = '.$status.', return_time = '.$return_time.' WHERE student_id = '.$student;
+			$db->query($query);
+		}else{
+			$query = 'UPDATE current SET status_id = '.$status.' WHERE student_id = '.$student;
+			$db->query($query);
+		}
+	}
+    return 0;
+}
+function enquote($text){
+	return '"'.$text.'"';
+}
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>
-      Attendance System
-    </title>
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  </head>
+  	<head>
+    	<title>
+				Attendence System
+    	</title>
+      <?php require_once('header.php'); ?>
+	    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    	<link rel="stylesheet" type="text/css" href="BREAKstyle.css">
+  	</head>
 	<body>
-    <div id="main-table">
-      <table>
-        <tr>
-          <th>
-            Student
-          </th>
-          <th>
-            Status
-          </th>
-        </tr>
-        <?php
-          $query = 'SELECT * FROM current INNER JOIN student_data ON current.student_id = student_data.student_id INNER JOIN status_data ON current.status_id = status_data.status_id ORDER BY first_name DESC';
-          $current = $db->query($query)->fetch_all($resulttype = MYSQLI_ASSOC);
-          foreach ($current as &$row) {
-            echo '<tr class="student-row" id="'.$row["student_id"].'">';
-            echo '<td>'.$row["first_name"].' '.$row["last_name"][0].'.</td>';
-            echo '<td><span class="status">'.$row["status_name"].'</span>';
-            echo '<input type="submit" name="1" value="P">';
-            echo '<input type="submit" name="7" value="A">';
-            echo '</td></tr>';
-          }
-        ?>
-      </table>
-    </div>
-    <div id="result">
-
-    </div>
-    <script type="text/javascript" src="js/changeStatus.js"></script>
+	<div class="container">
+	<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Location</th>
+      <th scope="col">Return Time</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"><input type="checkbox" class="checkbox"></th>
+      <td>Anthony Reyes</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row"><input type="checkbox" class="checkbox"></th>
+      <td></td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row"><input type="checkbox" class="checkbox"></th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</div>
 	</body>
 </html>
