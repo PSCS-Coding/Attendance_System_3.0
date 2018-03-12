40ad47a1fba552c442ba3624d14477ee2f38ab3d
jacknote <jack.notarangelo@pscs.org>
2018-03-12 09:44:06 -0700

    Fix Datetime.php
    This was done a week ago, and we can't remember what it was... but it fixed a bug in datetime

--- datetime.php
+++ datetime.php
@@ -5,31 +5,39 @@
   </head>
   <body>
     <?php
+		require_once('connection.php');
 		//TODO make this into function form
 		// TODO weekend or holiday if condition
-    function FunctionName($value='')
+    function elapsed_time($student_id)
     {
-    	# code...
+				global $db;
+
+				$lastEventTimeQuery = $db->query("SELECT timestamp FROM history WHERE student_id = '$student_id' LIMIT 1");
+				$time1 = new DateTime($lastEventTimeQuery->fetch_array()[0]); // last event in the history table
+				print_r($time1);
+				$time2 = new DateTime();
+
+				$start = new DateTime($time1->format('Y-m-d' . '9:00'));
+				$end = new DateTime($time2->format('Y-m-d' . '15:30'));
+				//is event 1 before the start of the school day of the same day?
+				if ($time1 < $start){
+		        $time1 = $start;
+		    }
+		        //is event 2 after the end of the school day of the same day of event 1?
+		    if ($time2 > $end){
+		        $time2 = $end;
+		    }
+				$time_elapsed = ($time2->getTimestamp()-$time1->getTimestamp())/60;
+				return $time_elapsed;
     }
-		$time1 = new DateTime("today 8:00");
-    $time2 = new DateTime("today 18:33");
-		$start = new DateTime($time1->format('Y-m-d') . "9:00");
-		$end = new DateTime($time2->format('Y-m-d') . "15:40");
+		echo elapsed_time(1);
 
-		if ($time1 <= $start){
-            $time1 = $start;
-        }
-        //is event 2 after the end of the school day of the same day of event 1?
-        if ($time2 >= $end){
-            $time2 = $end;
-        }
+    //echo $time1->format('Y-m-d H:i:s') . "<br/>";
+    //$time_elapsed = ($time2->getTimestamp()-$time1->getTimestamp())/60;   //echo round($time_elapsed, 3) . " Decimal Minutes<br/>";
+		//$time_seconds = round(($time_elapsed-floor($time_elapsed))*0.6, 2)*100;
+		//$time_minutes = floor($time_elapsed);
 
-    echo $time1->format('Y-m-d H:i:s') . "<br/>";
-    $time_elapsed = ($time2->getTimestamp()-$time1->getTimestamp())/60;   echo round($time_elapsed, 3) . " Decimal Minutes<br/>";
-		$time_seconds = round(($time_elapsed-floor($time_elapsed))*0.6, 2)*100;
-		$time_minutes = floor($time_elapsed);
-
-		echo $time_minutes . ":" . $time_seconds . " Minutes Seconds Time";
+		//echo $time_minutes . ":" . $time_seconds . " Minutes Seconds Time";
       ?>
 
     </body>
