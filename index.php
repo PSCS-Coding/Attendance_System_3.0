7dfcd8f378427a66e552060dc1a9c53d42113138
eta57 <theeta57@gmail.com>
2018-03-12 11:07:13 -0700

    Revert "Merge branch 'angus_branch' of https://github.com/PSCS-Coding/Attendance_System_3.0 into angus_branch"
    This reverts commit 43c8cb63b7a8575a8cfd33ab293724c6b73816c7, reversing
    changes made to f4fba995a9b25e2f53c1197492163abd66835357.

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ function enquote($text){
 
   <head>
 	<title>
-  		Attendance System
+  		Atendance�Sistim�100�Persent�Compleet�Perfict�耀No�Virus�Downlode�Free�Affective�end�Afficient�Profetional�Git�it�Now�Easy�Set�Up�Aply�Today�Has�Enyone�Really�Been�Far�Even�as�Descided�to�Use�Evin�Go�Wunt�to�do�Look�Mor�Like�Go�Further�You�Can�Realy�be�Far�It's�Just�Commin�Sense�Low�Price�Great�Deel�No�Charge�Limited�Time�Ofter
   	</title>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" type="text/css" href="style.css">
