2f3a68800b0bb3961722943edb979cb03c2b3955
henry956 <henry.bevil@PSCS.org>
2018-03-19 13:49:33 -0700

    stuff

--- admin.php
+++ admin.php
@@ -109,7 +109,6 @@ require_once("connection.php");
 			}
 			echo '</tr>';
 			foreach($values as $col => &$value){
-<<<<<<< HEAD
 				echo '<tr draggeble="'.$draggeble.'" ondragstart="drag(event)">';
 				echo '<td class="admin">'.$value[$index[0]].'</td>';
 				foreach($index as $row => &$oi){
@@ -126,7 +125,6 @@ require_once("connection.php");
               echo "</tr>";
             }
           }
-=======
 				echo '<tr>';
 				if((string)$_GET['page'] != "8"){
 					echo '<td class="admin">'.$value[$index[0]].'</td>';
@@ -139,7 +137,6 @@ require_once("connection.php");
 					foreach($index as $row => &$oi){
 						echo '<td class="admin"><form method="POST"><input type="text" name="new" class="newval" placeholder="'.$value[$oi].'"><input type="hidden" name="row" value="'.$row.'"><input type="hidden" name="col" value="'.$col.'"><input type="submit" name="go" class="submit" value="￭"></form></td>';
 					}
->>>>>>> ecd7f8ab0acffb43ca0c68195cf46cf4fe8e84b1
 				}
 				echo '</tr>';
 			}
