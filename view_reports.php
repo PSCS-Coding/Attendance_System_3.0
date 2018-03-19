5d7d9dd9722f151031185999cafef4db35b26a8d
jacknote <jack.notarangelo@pscs.org>
2018-03-19 14:42:32 -0700

    made offsite left var

--- view_reports.php
+++ view_reports.php
@@ -4,10 +4,16 @@ require_once('functions.php');
 
 
 function view_reports_for_student($student_id){
-    global $db
+    global $db;
     $OffsiteTimeUsed = total_offsite_used($student_id);
+    echo "Offsite Used: ", $OffsiteTimeUsed;
 
-    $query = "SELECT "
-    $OffsiteLeft = $OffsiteTimeUsed;
+    $query = "SELECT default_offsite FROM allotted_hours";
+    $result = $db->query($query);
+    $total_offsite = $result->fetch_array()[0] * 60;
+    $OffsiteLeft = $total_offsite - $OffsiteTimeUsed;
+    echo "<br/>Offsite Left: ", $OffsiteLeft;
 }
-  ?>
+
+view_reports_for_student(2);
+?>
