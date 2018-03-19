ee417c8e116a907b7c7d10313169d7486361909b
Nic Warmenhoven <nic@pscs.org>
2018-03-19 14:03:24 -0700

    Code cleanup

--- change_status.php
+++ change_status.php
@@ -12,36 +12,6 @@
       $time = '';
     }
 
-  // 	$len = strlen((string)$time);
-  // 	if($status == 5){
-	//  	$_POST['badnum'] = False;
-	//  	if ($time > 99) {
-	//   		$_POST['badnum'] = True;
-	//   	}else {
-	//   		do {
-	// 	  		$b = 1;
-	// 	  		if((int)((string)$time) > 3 && ($len - $b) % 2 == 0){
-	// 	  			$_POST['badnum'] = True;
-	// 	  		}
-	// 	  		$b++;
-	// 		} while ($b < $len);
-	//   	}
-	//   	if(!is_numeric($time) || ($time > 15.4 && $time < 100) || $time < 1 || $time > 1600 || $_POST['badnum']){
-	//   		if($_POST['new'] == 5){
-	//   			$time = 9;
-	//   		}
-	//   		else {
-	//   			$time = 15;
-	// 	  	}
-	//   	}
-	//   	if($time < 100){
-	//   		$time = $time * 100;
-	//   	}
-	//   	if($time < 900){
-	//   		$time = $time + 1200;
-	//   	}
-	//   	$time = $time * 100;
-	// }
   // need error handling on the following query
   status_update($student,$status,'',$time);
 
