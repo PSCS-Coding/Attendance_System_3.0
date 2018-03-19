efc90f2cbd01e3973f065035b03b192a59e1b3c6
eta57 <theeta57@gmail.com>
2018-03-19 10:14:19 -0700

    switched print_r

--- admin.php
+++ admin.php
@@ -6,7 +6,7 @@ require_once("connection.php");
 <html>
 <head>
   <title>
-	  Atendance�Sistim�100�Persent�Compleet�Perfict�耀No�Virus�Downlode�Free�Affective�end�Afficient�Profetional�Git�it�Now�Easy�Set�Up�Aply�Today�Has�Enyone�Really�Been�Far�Even�as�Descided�to�Use�Evin�Go�Wunt�to�do�Look�Mor�Like�Go�Further�You�Can�Realy�be�Far�It's�Just�Commin�Sense�Low�Price�Great�Deel�No�Charge�Limited�Time�Ofter
+	  Atendance�Sistim�100�Persent�Compleet�Perfict�No�Virus�Downlode�Free�Affective�end�Afficient�Profetional�Git�it�Now�Easy�Set�Up�Aply�Today�Has�Enyone�Really�Been�Far�Even�as�Descided�to�Use�Evin�Go�Wunt�to�do�Look�Mor�Like�Go�Further�You�Can�Realy�be�Far�It's�Just�Commin�Sense�Low�Price�Great�Deel�No�Charge�Limited�Time�Ofter
   </title>
   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
   <link rel="stylesheet" type="text/css" href="style.css">
@@ -67,11 +67,11 @@ require_once("connection.php");
 			}
 			$values = $db->query($query)->fetch_all($resulttype = MYSQLI_ASSOC);
 			echo '</tr>';
-			print_r($values);
+			print_r($query);
 			foreach($values as &$value){
 				echo '<tr>';
 				foreach($index as &$oi){
-					echo '<td>'.$value[$oi].'<td>';
+					echo '<td>'.$value[$oi].'</td>';
 				}
 				echo '</tr>';
 			}
