9a41975ffe78e0ac56c90b4508281643dadec297
jacknote <jack.notarangelo@pscs.org>
2018-03-26 11:08:03 -0700

    Fixed some master conflicts in functions

--- functions.php
+++ functions.php
@@ -2,21 +2,18 @@
 
 require_once('connection.php');
 
-function elapsed_time($student_id)
-{
+
+function elapsed($student_id) {
 		global $db;
-
-		function isWeekend($date) {
-				return (date('N', strtotime($date)) >= 6);
-		}
-
 
 		$lastEventTimeQuery = $db->query("SELECT timestamp FROM history WHERE student_id = '$student_id' ORDER BY event_id DESC LIMIT 1");
 		$time1 = new DateTime($lastEventTimeQuery->fetch_array()[0]); // last event in the history table
 		if (isWeekend($time1->format('Y-m-d'))) {
 			return 0;
 		}
-		if ($db->query("SELECT COUNT(*) FROM holidays WHERE holiday_date =" . $time1->format('Y-m-d'))) {
+
+		$holQuery = $db->query("SELECT COUNT(*) FROM holidays WHERE holiday_date =" . $time1->format('Y-m-d'));
+		if ($holQuery->fetch_array()[0] != 0) {
 			return 0;
 		}
 
@@ -28,7 +25,9 @@ function elapsed_time($student_id)
 		if ($time1 < $start){
 				$time1 = $start;
 		}
-				//is event 2 after the end of the school day of the same day of event 1?
+
+		//is event 2 after the end of the school day of the same day of event 1?
+
 		if ($time2 > $end){
 				$time2 = $end;
 		}
@@ -39,34 +38,61 @@ function elapsed_time($student_id)
 		return $time_elapsed;
 }
 
-function status_update($student, $status, $old_status, $info, $return_time)
-{
-		global $db;
-	  // Update current table with new event
-		$query = 'UPDATE current SET status_id = '.$status.' WHERE student_id = '.$student;
-		$db->query($query);
+function status_update($student, $status, $info = '', $return_time = '') {
 
-	  // Add new event to history table
-	  $query2 = 'INSERT INTO history (student_id, status_id, info, return_time) VALUES ('.$student.', '.$status.', '.$info.', '.$return_time.')';
-		$db->query($query2);
+	global $db;
+  // Update current table with new event
+	$query = 'UPDATE current SET status_id = '.$status.', return_time = "'.$return_time.'" WHERE student_id = '.$student;
+	$db->query($query);
 
-	  // TODO
-	  // (If old status is offsite, not checked in, absent, or late?) Update record in history table for student’s immediate prior event with calculated duration
+	// Update immediate prior record in history table with calculated duration
+	$elapsed = elapsed($student);
+  $query = "UPDATE history SET elapsed = '$elapsed' WHERE student_id = '$student' ORDER BY event_id DESC LIMIT 1";
+  $db->query($query);
+
+	// Add new event to history table
+  $query_insert = 'INSERT INTO history (student_id, status_id, return_time) VALUES ('.$student.', '.$status.', "'.$return_time.'")';
+	$db->query($query_insert);
 
 	  return 0;
-}
-function enquote($text){
-		return '"'.$text.'"';
 }
 
 function total_offsite_used($student){
 		global $db;
 
-		$query = "SELECT SUM(hrs_used) FROM history WHERE student_id = ".$student." AND (status_id = 0 OR status_id = 2 OR status_id = 4 OR status_id = 5 OR status_id = 7) LIMIT 500";
+		$query = "SELECT SUM(elapsed) FROM history WHERE student_id = ".$student." AND (status_id = 0 OR status_id = 2 OR status_id = 4 OR status_id = 5 OR status_id = 7) LIMIT 500";
 	  $result = $db->query($query);
 	  $sum = round($result->fetch_array()[0], 2);
 
 		return $sum;
 }
 
+function enquote($text) {
+	return '"'.$text.'"';
+}
+
+function isWeekend($date) {
+	return (date('N', strtotime($date)) >= 6);
+}
+
+function start_the_day() {
+	// Sets all active students to not checked in
+	global $db;
+	$query = 'SELECT timestamp FROM history ORDER BY event_ID DESC LIMIT 1';
+	$latest = $db->query($query)->fetch_all($resulttype = MYSQLI_ASSOC);
+	$latest = new DateTime($latest[0]['timestamp']);
+	$midnight = new DateTime('yesterday 23:59:59');
+	if ($latest < $midnight) {
+	  $query = 'SELECT student_id FROM student_data WHERE active = 1';
+	  $all_current_students = $db->query($query)->fetch_all();
+	  foreach ($all_current_students as $key => $value) {
+	    status_update($value[0],0,'');
+	  }
+	}
+}
+
+function pretty_time($SQLdatetime) {
+	$time = new DateTime($SQLdatetime);
+	return $time->format('g:i');
+}
 ?>
