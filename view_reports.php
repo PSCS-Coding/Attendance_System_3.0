19b5805554dbae46502d86a951dd2831f022c708
jacknote <jack.notarangelo@pscs.org>
2018-03-26 14:33:32 -0700

    Added an overly simplistic Late Counter
    Also added some TODO and the very beginnings of the new and improved late function
    
    Co-Authored-By: orcaswhale <orcaswhale@users.noreply.github.com>

--- view_reports.php
+++ view_reports.php
@@ -16,8 +16,39 @@ function view_reports_for_student($student_id){
     echo "<br/>Offsite Left: ", floor(($OffsiteLeft)/60), " Hours and ", $OffsiteLeft%60, " Minutes<br/>";
     $PercentOffsiteUsed = round(($OffsiteTimeUsed/$total_offsite)*100, 2) . '%';
     echo "<br/>Percent Offsite Used: ",$PercentOffsiteUsed;
-
-    #$NumLateEvents
+    get_all_lates($student_id);
 }
 
 view_reports_for_student(2);
+
+function get_all_lates($student_id) {
+    global $db;
+
+    $query = "SELECT * FROM history WHERE status_id= 5 AND student_id = " . $student_id;
+    $result = $db->query($query);
+    //for ($i=0; $i < strlen($result->fetch_array()); $i++) {
+    //   # code...
+    //}
+    /*  Milo gave us this little gem. The code below was totally copied shamelessly from him, but it gets the result of the above query
+    structured line by line and printed somewhat neatly below.  */
+    $statusData = array();
+    while ($stat_row = $result->fetch_assoc()) {
+      array_push ($statusData, $stat_row);
+    }
+    echo("<pre>");
+    print_r($statusData);
+    echo("</pre>");
+
+    // TODO: change the above function such that it only counts ONE LATE PER DAY
+
+    /* TODO:  add in the 'Actually Late' function below (it will only count one per day, and each one will mean that the student was
+    (expectedly or no) late that day, arriving after 9:00am*/
+    // IDEA: for each late event check if the prior late event was on the same day
+    // IDEA: for each late event check if the next event is after the start of school
+    $result = $db->query("SELECT * FROM history WHERE status_id= 5 AND student_id =" . $student_id)
+
+    $query2 = "SELECT";
+    $NumLateEvents = 0;
+    return 0;
+
+}
