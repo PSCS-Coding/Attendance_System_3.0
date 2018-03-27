e5958b5bd5ee7d9678c2f2fcb0ce03efc469312d
eta57 <theeta57@gmail.com>
2018-03-26 19:54:26 -0700

    My very unfinished draft of group.php
    feel free to revert all the changes I've made, if I don't make this work Henry.

--- group.php
+++ group.php
@@ -2,33 +2,114 @@
 <html>
   <head>
     <title>Group Edit View</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-  </head>
-  <body class="top">
-    <?php
-     require_once("connection.php");
-      $query = "SELECT first_name, last_name FROM student_data WHERE active = 1";
-      $values = $db->query($query)->fetch_all($resulttype = MYSQLI_ASSOC);
-      $foo = count($values);
-    ?>
-    <script>
-    var group = [];
-      function allowDrop(ev) {
-          ev.preventDefault();
-      }
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <title>jQuery UI Droppable - Simple photo manager</title>
+	  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	  <link rel="stylesheet" href="/resources/demos/style.css">
+	  <style>
+	  #gallery { float: left; width: 65%; min-height: 12em; }
+	  .gallery.custom-state-active { background: #eee; }
+	  .gallery li { float: left; width: 96px; padding: 0.4em; margin: 0 0.4em 0.4em 0; text-align: center; }
+	  .gallery li h5 { margin: 0 0 0.4em; cursor: move; }
+	  .gallery li a { float: right; }
+	  .gallery li a.ui-icon-zoomin { float: left; }
+	  .gallery li img { width: 100%; cursor: move; }
 
-      function drag(ev) {
-          ev.dataTransfer.setData("text", ev.target.id);
-      }
-      function drop(ev) {
-        ev.preventDefault();
-        var data = ev.dataTransfer.getData("text");
-        if (confirm("Add " + document.getElementById(data).textContent + " to group")) {
-          group.push(document.getElementId(data)[0].textContent);
-          ev.target.appendChild(document.getElementById(data));
-        }
-      }
-  </script>
+	  #trash { float: right; width: 32%; min-height: 18em; padding: 1%; }
+	  #trash h4 { line-height: 16px; margin: 0 0 0.4em; }
+	  #trash h4 .ui-icon { float: left; }
+	  #trash .gallery h5 { display: none; }
+	  </style>
+	  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	  <script>
+	  $( function() {
+
+	    // There's the gallery and the trash
+	    var $gallery = $( "#gallery" ),
+	      $trash = $( "#trash" );
+
+	    // Let the gallery items be draggable
+	    $( "li", $gallery ).draggable({
+	      cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+	      revert: "invalid", // when not dropped, the item will revert back to its initial position
+	      containment: "document",
+	      helper: "clone",
+	      cursor: "move"
+	    });
+
+	    // Let the trash be droppable, accepting the gallery items
+	    $trash.droppable({
+	      accept: "#gallery > li",
+	      classes: {
+	        "ui-droppable-active": "ui-state-highlight"
+	      },
+	      drop: function( event, ui ) {
+	        deleteImage( ui.draggable );
+	      }
+	    });
+
+	    // Let the gallery be droppable as well, accepting items from the trash
+	    $gallery.droppable({
+	      accept: "#trash li",
+	      classes: {
+	        "ui-droppable-active": "custom-state-active"
+	      },
+	      drop: function( event, ui ) {
+	        recycleImage( ui.draggable );
+	      }
+	    });
+
+	    // Image deletion function
+	    function deleteImage( $item ) {
+	      $item.fadeOut(function() {
+	        var $list = $( "ul", $trash ).length ?
+	          $( "ul", $trash ) :
+	          $( "<ul class='gallery ui-helper-reset'></ul>" ).appendTo( $trash );
+
+	        $item.find( "a.ui-icon-trash" ).remove();
+	        $item.appendTo( $list ).fadeIn(function() {
+	          $item
+	            .animate({ width: "96px" })
+	              .animate({ height: "25px" });
+	        });
+	      });
+	    }
+
+	    // Image recycle function
+	    function recycleImage( $item ) {
+	      $item.fadeOut(function() {
+	        $item
+	          .find( "a.ui-icon-refresh" )
+	            .remove()
+	          .end()
+	          .css( "width", "96px")
+	          .find( "img" )
+	            .css( "height", "72px" )
+	          .end()
+	          .appendTo( $gallery )
+	          .fadeIn();
+	      });
+	    }
+
+	  } );
+	  </script>
+	</head>
+	<body class="top">
+      <?php
+      	require_once("connection.php");
+  		$query = "SELECT first_name, last_name FROM student_data WHERE active = 1";
+  		$values = $db->query($query)->fetch_all($restablettype = MYSQLI_ASSOC);
+  		$foo = count($values);
+      ?>
+
+
+
     <div class = "sidebar">
   	admin
   	<a class= "sidetext" href="/admin.php?page=0">Allotted Hours</a>
@@ -45,22 +126,29 @@
   	<a class= "sidetext" href="/index.php">Front Page</a>
 	<a class= "sidetext" href="statusview.php">Status View</a>
   	</div>
-  <div id="div2" ondrop="drop(event)" ondragover="allowDrop(event)"><p id="div1">test2</p></div>
-  <div id="drag1">
-    <table>
-      <tbody>
-        <th id="t1">students</th>
+
+
+	<div class="ui-widget ui-helper-clearfix">
+
+	<div id="trash" class="ui-widget-content ui-state-default">
+	  <h4> group</h4>
+	</div>
+	<div class="droppable">
+	<ul id="gallery" class="gallery ui-helper-reset ui-helper-clearfix">
+
         <?php
           for ($i=0; $i < $foo; $i++) {
-            $x = $i;
-            if($i == 1) {
-              $x++;
-            }
-              echo "<tr id='".$i."' draggable='true' ondragstart='drag(event)'><td id='".$x."'>".$values[$i]['first_name']."</td></tr>";
+              echo '<li class="ui-widget-content ui-corner-tr"><h5 class="ui-widget-header">'.$values[$i]['first_name'].' '.$values[$i]['last_name'][0].'.</h5></li>';
+
           }
          ?>
-     </tbody>
-    </table>
-  </div>
+
+	</ul>
+
+</div>
+
+	</div>
+
+
   </body>
 </html>
