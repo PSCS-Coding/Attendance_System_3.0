2bd7b9c039e9c6eb34fad2a5fdd85bb77ae7e2a5
eta57 <theeta57@gmail.com>
2018-03-30 12:06:46 -0700

    better links

--- group.php
+++ group.php
@@ -96,18 +96,18 @@
 
     <div class = "sidebar">
   	admin
-  	<a class= "sidetext" href="/admin.php?page=0">Allotted Hours</a>
-  	<a class= "sidetext" href="/admin.php?page=1">Current Events</a>
-  	<a class= "sidetext" href="/admin.php?page=2">Facilitator Edit View</a>
-  	<a class= "sidetext" href="/admin.php?page=3">Group Edit View</a>
-  	<a class= "sidetext" href="/admin.php?page=4">History</a>
-  	<a class= "sidetext" href="/admin.php?page=5">Holidays</a>
-  	<a class= "sidetext" href="/admin.php?page=6">Offsite Locations</a>
-  	<a class= "sidetext" href="/admin.php?page=7">Passwords</a>
-  	<a class= "sidetext" href="/admin.php?page=8">School Hours</a>
-  	<a class= "sidetext" href="/admin.php?page=9">Student Edit View</a>
+  	<a class= "sidetext" href="admin.php?page=0">Allotted Hours</a>
+  	<a class= "sidetext" href="admin.php?page=1">Current Events</a>
+  	<a class= "sidetext" href="admin.php?page=2">Facilitator Edit View</a>
+  	<a class= "sidetext" href="admin.php?page=3">Group Edit View</a>
+  	<a class= "sidetext" href="admin.php?page=4">History</a>
+  	<a class= "sidetext" href="admin.php?page=5">Holidays</a>
+  	<a class= "sidetext" href="admin.php?page=6">Offsite Locations</a>
+  	<a class= "sidetext" href="admin.php?page=7">Passwords</a>
+  	<a class= "sidetext" href="admin.php?page=8">School Hours</a>
+  	<a class= "sidetext" href="admin.php?page=9">Student Edit View</a>
   	front end
-  	<a class= "sidetext" href="/index.php">Front Page</a>
+  	<a class= "sidetext" href="index.php">Front Page</a>
 	<a class= "sidetext" href="statusview.php">Status View</a>
   	</div>
 
