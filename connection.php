d5ab648ad114cb7fa934a9bfbaa50d2d80ef3aaf
eta57 <35232264+eta57@users.noreply.github.com>
2018-04-16 18:19:38 -0700

    removed git stuff

--- connection.php
+++ connection.php
@@ -1,13 +1,7 @@
 <?php
-<<<<<<< HEAD
-	//$db = new mysqli('localhost', 'root', 'root', 'attendance_new');
-    $db = new mysqli('pscscoding.com', 'rabu870', '12345', 'pscsorg_3', '3306');
-=======
-	//$db = new mysqli('localhost', 'root', '', 'attendance_system');
-	//$db = new mysqli('localhost', 'root', '', 'elispscsorg');
 	$db = new mysqli('localhost', 'root', 'root', 'elispscsorg');
+	//$db = new mysqli('localhost', 'root', '', 'attendance_system');
     //$db = new mysqli('pscscoding.com', 'rabu870', '12345', 'pscsorg_3', '3306');
->>>>>>> angus_branch
     if($db->connect_errno > 0){
         echo 'fail';
         die('Unable to connect to database [' . $db->connect_error . ']');
