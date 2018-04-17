5ccde8131481321111887d51b091c50f460d91ce
eta57 <theeta57@gmail.com>
2018-04-16 18:29:35 -0700

    previous unresolved merge conflict

--- index.php
+++ index.php
@@ -53,27 +53,6 @@ start_the_day();
          	$query = 'SELECT * FROM current INNER JOIN student_data ON current.student_id = student_data.student_id INNER JOIN status_data ON current.status_id = status_data.status_id WHERE student_data.active = 1 ORDER BY first_name DESC';
          	$current = $db->query($query)->fetch_all($resulttype = MYSQLI_ASSOC);
          	foreach ($current as &$row) {
-            	echo '<tr class="student-row" id="'.$row["student_id"].'">';
-            	echo '<td>'.$row["first_name"].' '.$row["last_name"][0].'.</td>';
-            	echo '<td><span class="status">'.$row["status_name"];
-							if($row["status_name"] == "Late") {
-								echo " arriving at ".pretty_time($row["return_time"]);
-							}
-							echo '</span>';
-							if($row['status_id'] != 1 ) { // if not present, show the present button
-								echo '<input type="submit" name="1" value="P">';
-			  					if($row['status_id'] == 0 || $row['status_id'] == 5) { // if not checked in or late, show late fields
-										echo '<input name="time" type="text" class="late" placeholder="Arrival time"><input type="submit" name="5" value="L">';
-			  					}
-			  					if($row['status_id'] != 7  && $row['status_id'] != 4) { // if not absent or checked out, show absent button
-					            	echo '<input type="submit" name="7" value="A">';
-			  					}
-			  				}
-		  				else { // student is present
-		  					if($row['status_id'] != 4 ) { // if not checked out, show check out button
-				            	echo '<input type="submit" name="4" value="CO">';
-		  					}
-		  				}
             	echo '<tr class="student-row" id="'.$row["student_id"].'"><td>'.$row["first_name"].' '.$row["last_name"][0].'.</td><td><span class="status">'.$row["status_name"];
 				if($row["status_name"] == "Late"){
 					if((int)(($row["return_time"][0].$row["return_time"][1])) > 12){
