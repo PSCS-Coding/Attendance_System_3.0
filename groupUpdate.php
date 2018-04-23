93c791851cfdb606c0a12b80a395764cb95ab52c
henry956 <henry.bevil@PSCS.org>
2018-04-23 14:43:52 -0700

    cool working stuff

--- groupUpdate.php
+++ groupUpdate.php
@@ -1,7 +1,12 @@
 <?php
+  require_once("connection.php");
   $_POST["group"];
-  $update = "";
+  $_POST["name"];
+  $arr = array();
   foreach ($_POST["group"] as $x => $i) {
-    
+    $leng = $_POST["group"][$i].length - 2;
+    $query = $db->query("SELECT student_id FROM student_data WHERE first_name = ".$_POST['group'][$i][$leng]." AND last_name = ".substr($_POST['group'][$i], $leng));
+    $arr.push($query);
   }
+$insertQuery = $db->query("INSERT INTO groups (group_name, students) VALUES (".$_POST['name'].", ".implode(',', $arr).")");
 ?>
