5399932175ec852560fc1411a6e26e9ab47d3dbe
CadenWellander <35236380+CadenWellander@users.noreply.github.com>
2018-04-26 09:47:28 -0700

    shortaning status view

--- statusview.php
+++ statusview.php
@@ -52,110 +52,36 @@ Status View
 							}
 							$x = $x + 1;
             }
-	?>
-	<div>
-		<h3>
-			Not Checked In:
-		</h3>
-		<?php
-			$arrlength = count($not_checked_in);
-			for($x = 0; $x < $arrlength; $x++) {
-				echo "<ul>";
-    			echo "<li>"."$not_checked_in[$x]"."</li>";
-				echo "</ul>";
-			}
-		?>
-	</div>
-	<div>
-		<h3>
-			Present:
-		</h3>
-		<?php
-			$arrlength = count($present);
-			for($x = 0; $x < $arrlength; $x++) {
-				echo "<ul>";
-					echo "<li>"."$present[$x]"."</li>";
-				echo "</ul>";
-			}
-		?>
-	</div>
-	<div>
-		<h3>
-			Offsite:
-		</h3>
-		<?php
-			$arrlength = count($offsite);
-			for($x = 0; $x < $arrlength; $x++) {
-				echo "<ul>";
-					echo "<li>"."$offsite[$x]"."</li>";
-				echo "</ul>";
-			}
-		?>
-	</div>
-	<div>
-		<h3>
-			Field Trip:
-		</h3>
-		<?php
-			$arrlength = count($field_trip);
-			for($x = 0; $x < $arrlength; $x++) {
-				echo "<ul>";
-					echo "<li>"."$field_trip[$x]"."</li>";
-				echo "</ul>";
-			}
-		?>
-	</div>
-	<div>
-		<h3>
-			Checked Out:
-		</h3>
-		<?php
-			$arrlength = count($checked_out);
-			for($x = 0; $x < $arrlength; $x++) {
-				echo "<ul>";
-					echo "<li>"."$checked_out[$x]"."</li>";
-				echo "</ul>";
-			}
-		?>
-	</div>
-	<div>
-		<h3>
-			Late:
-		</h3>
-		<?php
-			$arrlength = count($late);
-			for($x = 0; $x < $arrlength; $x++) {
-				echo "<ul>";
-					echo "<li>"."$late[$x]"."</li>";
-				echo "</ul>";
-			}
-		?>
-	</div>
-	<div>
-		<h3>
-			Independent Study:
-		</h3>
-		<?php
-			$arrlength = count($independent_study);
-			for($x = 0; $x < $arrlength; $x++) {
-				echo "<ul>";
-					echo "<li>"."$independent_study[$x]"."</li>";
-				echo "</ul>";
-			}
-		?>
-	</div>
-	<div>
-		<h3>
-			Absent:
-		</h3>
-		<?php
-			$arrlength = count($absent);
-			for($x = 0; $x < $arrlength; $x++) {
-				echo "<ul>";
-					echo "<li>"."$absent[$x]"."</li>";
-				echo "</ul>";
-			}
-		?>
-	</div>
+						$master_array[0] = $not_checked_in;
+						$master_array[1] = $present;
+						$master_array[2] = $offsite;
+						$master_array[3] = $field_trip;
+						$master_array[4] = $checked_out;
+						$master_array[5] = $late;
+						$master_array[6] = $independent_study;
+						$master_array[7] = $absent;
+						$master_array_names[0] = "Not Checked In:";
+						$master_array_names[1] = "Present:";
+						$master_array_names[2] = "Offsite:";
+						$master_array_names[3] = "Field Trip:";
+						$master_array_names[4] = "Checked Out:";
+						$master_array_names[5] = "Late:";
+						$master_array_names[6] = "Independent Study:";
+						$master_array_names[7] = "Absent:";
+						$masterarrlength = count($master_array);
+						for($b = 0; $b < $masterarrlength; $b++) {
+							echo "<div>";
+								echo "<h3>";
+									echo $master_array_names[$b];
+								echo "</h3>";
+								$arrlength = count($master_array[$b]);
+								for($x = 0; $x < $arrlength; $x++) {
+									echo "<ul>";
+    								echo "<li>".$master_array[$b][$x]."</li>";
+									echo "</ul>";
+								}
+							echo "</div>";
+						}
+?>
 </body>
 </html>
