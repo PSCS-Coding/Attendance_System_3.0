ef14ad9db1ba677f13352b8f664f944acf2efcb5
Rabu <eli@kimhi.com>
2018-05-15 08:31:12 -0700

    Selecting students, sytax fixes, prep for data from db, removing files I'm not working with

--- addtogroup.php
+++
@@ -1,7 +0,0 @@
-<?php
-  	require_once("connection.php");
-	$values = $db->query('SELECT * FROM groups WHERE group_name = "'.$_POST['group'].'"')->fetch_all($resulttype = MYSQLI_ASSOC);
-	$q = 'UPDATE groups SET students = "'.$values[0]['students'].','.$_POST['student'].'" WHERE group_name = "'.$_POST['group'].'"';
-	$db->query($q)->fetch_all($resulttype = MYSQLI_ASSOC);
-	print_r($q);
-?>
--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
 Vue.component('student', {
-    props: ['firstName', 'lastName', 'returnTime', 'info', 'status'],
+    props: {
+        studentId: Number,
+        firstName: String,
+        lastName: String,
+        returnTime: String,
+        info: String,
+        status: Number
+    },
     template: `
         <div class='student'>
-            <p>
-                {{ firstName }} {{ [...lastName][0] }}. is {{ status }}
+            <input type='checkbox' :value='studentId' :id='studentId' v-model='$root.selected'>
+            <label :for='studentId'>
+                {{ firstName }} {{ [...lastName][0] }}. is {{ $root.statusData[status] }}
                 <span v-if='info'> at/with {{ info }}, returning at {{ returnTime }}</span>
-            </p>
+            </label>
         </div>`
 });
 
 Vue.component('student-list', {
     template: `
         <div class='student-list'>
-            <student v-for='student of $root.students' :key='student.studentId' :first-name='student.firstName' :last-name='student.lastName' :status='$root.status_data[student.status]' :return-time='student.returnTime' :info='student.info'></student>
+            <student v-for='student of $root.students' :key='student.studentId' :student-id='student.studentId' :first-name='student.firstName' :last-name='student.lastName' :status='student.status' :return-time='student.returnTime' :info='student.info'></student>
         </div>`
 });
 
@@ -21,7 +29,7 @@ Vue.component('student-list', {
 var vm = new Vue({
     el: '#attendance',
     data: {
-        students: [{
+        students: [{ //will be a blank array
                 studentId: 1,
                 firstName: 'Eli',
                 lastName: 'Kimchi',
@@ -36,7 +44,7 @@ var vm = new Vue({
                 status: 7
             }
         ],
-        status_data: [
+        statusData: [ //will be a blank array
             'Not Checked In',
             'Present',
             'Offsite',
@@ -45,6 +53,25 @@ var vm = new Vue({
             'Late',
             'Independent Study',
             'Absent'
-        ]
+        ],
+        selected: []
+    },
+    methods: {
+        /*
+        load: function() {
+            //get student info from the database and create an object for each student in the students array
+            //get status info from the database and put it into the statusData array
+            //get current info
+        }
+        */
     }
+    /*,
+        beforeMount() {
+            //create a global load function that fetches students and their data from the db, as well as status data and info from the current table.
+            this.load();
+        },
+        mounted() {
+            //create a general purpose listen (name?) function that will listen for anything that needs to be dealing with the database.
+            this.listen();
+        }*/
 });
